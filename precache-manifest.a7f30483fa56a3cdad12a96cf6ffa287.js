8661d7468531796e352ea26a60f35042d8c0ff41
Travis Mendenhall <twmwayne@gmail.com>
2021-03-19 15:22:36 -0600

    Updates

---
+++ precache-manifest.a7f30483fa56a3cdad12a96cf6ffa287.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a37eeac5fd5050821deebd22a300d5",
+    "url": "/travis-employee-directory1/index.html"
+  },
+  {
+    "revision": "abe9069e02f7c8283194",
+    "url": "/travis-employee-directory1/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "a4ce8f9a452a5f38da81",
+    "url": "/travis-employee-directory1/static/css/main.66e98012.chunk.css"
+  },
+  {
+    "revision": "abe9069e02f7c8283194",
+    "url": "/travis-employee-directory1/static/js/2.2a2dc6f6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/travis-employee-directory1/static/js/2.2a2dc6f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4ce8f9a452a5f38da81",
+    "url": "/travis-employee-directory1/static/js/main.e1cc54be.chunk.js"
+  },
+  {
+    "revision": "899c4c4916b14dbe9643",
+    "url": "/travis-employee-directory1/static/js/runtime-main.c0c6b886.js"
+  }
+]);
